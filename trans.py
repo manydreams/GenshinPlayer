00d2c1530a4fe254275ebba38a00d69448391231
manydreams <2836145041@qq.com>
2025-04-28 01:07:07 +0800

    change the return format of the midi_to_lyre func

--- trans.py
+++ trans.py
@@ -1,7 +1,16 @@
-from mido import MidiFile, MidiTrack, Message
+from mido import MidiFile, MidiTrack, Message, tempo2bpm
 
-def midi_to_melody(file_path) -> list[(float, str, bool)]:
-    """read midi file and return a list of `(time, note, is_on)` tuples.
+def midi_to_lyre(file_path: str, offset: int = 0) -> dict:
+    """read midi file and return a dict string of melody.
+    Args:
+        file_path (str): path of the midi file.
+    
+    Returns:
+        a dict like:
+        {
+            "melody": [[time(float), note(str), is_on(bool)], [...], [...],...],
+            "bpm": float | None
+        }
     """
     key_map = {
         48: 'C4', 50: 'D4', 52: 'E4', 53: 'F4',
@@ -14,14 +23,32 @@ def midi_to_melody(file_path) -> list[(float, str, bool)]:
     ret = []
     mid = MidiFile(file_path)
     cur = 0.0
-    for msg in mid.tracks[3]:
+    def get_track():
+        for track in mid.tracks:
+            for msg in track:
+                if msg.type == 'note_on' or msg.type == 'note_off':
+                    return track
+    def get_time_signature():
+        for track in mid.tracks:
+            for msg in track:
+                if msg.type == 'time_signature':
+                    return (msg.numerator, msg.denominator)
+        return (4, 4)
+    def get_bpm():
+        for track in mid.tracks:
+            for msg in track:
+                if msg.type == 'set_tempo':
+                    return tempo2bpm(msg.tempo, get_time_signature())
+        return None
+    for msg in get_track():
         cur += msg.time
         
         match msg.type:
+            # case 'note_on' | 'note_off':
             case 'note_on':
-                if msg.note in key_map:
-                    ret.append((cur/mid.ticks_per_beat, key_map[msg.note], True))
-            case 'note_off':
-                if msg.note in key_map:
-                    ret.append((cur/mid.ticks_per_beat, key_map[msg.note], False))
-    return ret
+                note = msg.note + offset
+                # if note in key_map:
+                ret.append((cur/mid.ticks_per_beat, key_map[note], True))
+                ret.append((cur/mid.ticks_per_beat, key_map[note], False))
+                    
+    return {'melody': ret, 'bpm': get_bpm()}
